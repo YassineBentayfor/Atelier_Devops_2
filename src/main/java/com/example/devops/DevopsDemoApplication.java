44a58e651fd748a18ef897abaa35c5a7ea0c1810
Yassine Bentayfor <yassine.bentayfor2015@gmail.com>
2024-04-24 09:43:29 +0100

    first commit

---
+++ src/main/java/com/example/devops/DevopsDemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.devops;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDemoApplication.class, args);
+	}
+	@PostConstruct
+	public void onApplicationStart() {
+		System.out.println("Spring Boot application started successfully!");
+	}
+
+}
